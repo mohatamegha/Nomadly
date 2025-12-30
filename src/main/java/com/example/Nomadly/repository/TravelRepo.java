076504649c227e74f6ce15dad564ef9466f8435e
kothari-vanshika <vanshikakothari524@gmail.com>
2025-12-30 18:18:02 +0530

    updated Travel repo

--- src/main/java/com/example/Nomadly/repository/TravelRepo.java
+++ src/main/java/com/example/Nomadly/repository/TravelRepo.java
@@ -2,11 +2,13 @@ package com.example.Nomadly.repository;
 
 import com.example.Nomadly.entities.Travel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TravelRepo extends JpaRepository<Travel, Long> {
+    @Query(value="SELECT * FROM travel WHERE LOWER(destination) LIKE LOWER(CONCAT('%',:destination,'%'))")
     List<Travel> findByDestination(String destination);
 }
