1fbdea97393402c573603130b2939b69407177ad
kothari-vanshika <vanshikakothari524@gmail.com>
2025-12-31 11:01:20 +0530

    added chat related files

--- src/main/java/com/example/Nomadly/config/WebSocketConfig.java
+++ src/main/java/com/example/Nomadly/config/WebSocketConfig.java
@@ -1,7 +1,7 @@
 package com.example.Nomadly.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+
 import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
